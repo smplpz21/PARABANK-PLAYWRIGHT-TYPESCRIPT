e9cd7cabd7259baf9e724d922bfe969209c89c66
samfinch14 <samfinch14@gmail.com>
2025-05-13 12:43:01 +0800

    add jsdoc comment

--- src/services/FindTransctionService.ts
+++ src/services/FindTransctionService.ts
@@ -1,11 +1,17 @@
-import { APIRequestContext } from '@playwright/test';
+import { APIRequestContext, APIResponse } from '@playwright/test';
 
 export default class FindTransctionService {
 	constructor(private readonly apiContext: APIRequestContext) {
 		this.apiContext = apiContext;
 	}
 
-	async findTransactionByAmount(accountId: string, paymentAmount: string) {
+	/**
+	 * Fetches transactions for a specific account based on the transaction amount.
+	 * @param {string} accountId - The ID of the account to search transactions for.
+	 * @param {string} paymentAmount - The transaction amount to filter by.
+	 * @returns {Promise<APIResponse>} The API response containing the transaction details.
+	 */
+	async findTransactionByAmount(accountId: string, paymentAmount: string): Promise<APIResponse> {
 		return this.apiContext.get(
 			`parabank/services_proxy/bank/accounts/${accountId}/transactions/amount/${paymentAmount}?timeout=30000`
 		);
